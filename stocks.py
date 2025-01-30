82ba9817e4a29424be533c6b002f8f01f0d3a40c
Titiksha <tb8398@srmist.edu.in>
2025-01-30 18:43:36 +0530

    clearer graph representation

--- stocks.py
+++ stocks.py
@@ -7,8 +7,13 @@ data = yf.download(stock_symbol, start="2020-01-01", end="2025-01-01")
 
 # Plot the closing price
 plt.figure(figsize=(10,6))
-plt.plot(data['Close'], label=f'{stock_symbol} Closing Price')
-plt.title(f'{stock_symbol} Stock Price Over Time')
+plt.plot(data['Close'], label=f'{stock_symbol} Closing Price is')
+
+max_price = data['Close'].max()
+max_date = data['Close'].idxmax()
+plt.scatter(max_date, max_price, color='red', label=f'Max Price: {max_price:.2f}')
+
+plt.title(f'{stock_symbol} Stock Price Over Time is')
 plt.xlabel('Date')
 plt.ylabel('Price (USD)')
 plt.legend()
